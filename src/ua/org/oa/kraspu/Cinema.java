154378911f5875f5c29665345b992103e5a892d7
Pavlo <kraspu@gmail.com>
2018-11-27 14:26:31 +0200

    Cinema error

--- src/ua/org/oa/kraspu/Cinema.java
+++ src/ua/org/oa/kraspu/Cinema.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Cinema {
   ArrayList<Movies> movieList = new ArrayList<>();
   Movies mov1 = new Movies("\tФантастичнi звiрi: Злочини Грiндельвальда", "Adventure, Family, Fantasy",136, 5);
+  movieList.add(mov1);
+  movieList.add(new Movies("\t\tБогемна рапсодiя", " Biography, Drama, Music",134, 0));
 
-  Movies mov2 = new Movies("\tФантастичнi звiрi: Злочини Грiндельвальда", "Adventure, Family, Fantasy",136, 5);
+  public void infoList() {
+    for (Movies li : movieList) {
+      System.out.println(li);
+    }
+  }
 }
