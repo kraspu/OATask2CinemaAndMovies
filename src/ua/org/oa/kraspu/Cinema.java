c3939ee9c93e9ade86883d157e6b5eec989bb5ce
Pavlo <kraspu@gmail.com>
2018-11-27 15:02:41 +0200

    Movie.java: error in setRateCount(int rateCount)

--- src/ua/org/oa/kraspu/Cinema.java
+++ src/ua/org/oa/kraspu/Cinema.java
@@ -2,11 +2,18 @@ package ua.org.oa.kraspu;
 
 import java.util.ArrayList;
 
+
+
 public class Cinema {
-  ArrayList<Movies> movieList = new ArrayList<>();
-  Movies mov1 = new Movies("\tФантастичнi звiрi: Злочини Грiндельвальда", "Adventure, Family, Fantasy",136, 5);
-  movieList.add(mov1);
-  movieList.add(new Movies("\t\tБогемна рапсодiя", " Biography, Drama, Music",134, 0));
+
+  private ArrayList<Movies> movieList = new ArrayList<>();
+
+
+
+  public void addMovie(Movies mov) {
+      movieList.add(mov);
+  }
+
 
   public void infoList() {
     for (Movies li : movieList) {
--- src/ua/org/oa/kraspu/Main.java
+++ src/ua/org/oa/kraspu/Main.java
@@ -1,4 +1,25 @@
 package ua.org.oa.kraspu;
 
 public class Main {
+
+
+  public static void main(String[] args) {
+
+    Cinema pravdaKino = new Cinema();
+
+    pravdaKino.addMovie(new Movies("\tФантастичнi звiрi: Злочини Грiндельвальда", "Adventure, Family, Fantasy", 136, 5));
+    pravdaKino.infoList();
+
+
+
+
+
+
+
+
+
+    //    Movies mov1 = new Movies("\tФантастичнi звiрi: Злочини Грiндельвальда", "Adventure, Family, Fantasy", 136, 5);
+//    movieList.add(mov1);
+    // movieList.add(new Movies("\t\tБогемна рапсодiя", " Biography, Drama, Music",134, 0));
+  }
 }
